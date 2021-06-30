b7dbd43af40a64ea414d0897fea7d1dc95159edb
Ulli-H <uanklam@yahoo.de>
2021-06-30 22:33:45 +0200

    adding queries and create_table.py

---
+++ sql_queries.py
@@ -0,0 +1,112 @@
+import configparser
+
+
+# CONFIG
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+
+# DROP TABLES
+
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
+
+# CREATE TABLES
+
+staging_events_table_create= ("""
+""")
+
+staging_songs_table_create = ("""
+""")
+
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays(
+                                songplay_id SERIAL UNIQUE,
+                                start_time bigint NOT NULL REFERENCES time,
+                                user_id int NOT NULL REFERENCES users,
+                                level varchar, 
+                                song_id varchar REFERENCES songs,
+                                artist_id varchar REFERENCES artists,
+                                session_id int, 
+                                location varchar, 
+                                user_agent varchar,
+                                PRIMARY KEY (start_time, user_id, song_id,
+                                artist_id))
+""")
+
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int PRIMARY KEY,
+                                                        first_name varchar NOT NULL,
+                                                        last_name varchar NOT NULL,
+                                                        gender varchar,
+                                                        level varchar NOT NULL)
+""")
+
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY,
+                                                        title varchar NOT NULL, 
+                                                        artist_id varchar NOT NULL,
+                                                        year int NOT NULL,
+                                                        duration float NOT NULL)
+""")
+
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY,
+                                                              name varchar NOT NULL,
+                                                              location varchar,
+                                                              latitude float,
+                                                              longitude float)
+""")
+
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time bigint PRIMARY KEY,
+                                                        hour int NOT NULL,
+                                                        day int NOT NULL,
+                                                        week int NOT NULL,
+                                                        month int NOT NULL,
+                                                        year int NOT NULL,
+                                                        weekday varchar NOT NULL)
+""")
+
+# STAGING TABLES
+
+staging_events_copy = ("""
+""").format()
+
+staging_songs_copy = ("""
+""").format()
+
+# FINAL TABLES
+
+songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, 
+                                                   level, song_id, artist_id,
+                                                   session_id, location,
+                                                   user_agent) 
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s);""")
+
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, 
+                                           gender, level) 
+                        VALUES (%s, %s, %s, %s, %s) \
+                        ON CONFLICT (user_id) DO UPDATE
+                                        SET level = EXCLUDED.level;""")
+
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, 
+                                           duration) 
+                        VALUES (%s, %s, %s, %s, %s);""")
+
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, 
+                                               latitude, longitude) 
+                          VALUES (%s, %s, %s, %s, %s) 
+                          ON CONFLICT (artist_id) DO NOTHING;""")
+
+
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month,
+                                          year, weekday) 
+                        VALUES (%s, %s, %s, %s, %s, %s, %s) 
+                        ON CONFLICT (start_time) DO NOTHING;""")
+
+# QUERY LISTS
+
+create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+copy_table_queries = [staging_events_copy, staging_songs_copy]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
